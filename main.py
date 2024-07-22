9a1043c77c3b3d3c6f5b4c2af322ede41ae5d64c
DeivisFelipe <guerreirodosul01@gmail.com>
2024-07-22 12:55:19 -0300

    fix: acessando um trabalho por vez

--- main.py
+++ main.py
@@ -24,21 +24,24 @@ app.add_middleware(
 )
 
 # Importa e inclui os routers dos módulos
-modules_path = "Modules"
-modules = [f for f in listdir(modules_path) if not isfile(join(modules_path, f))]
-for module in modules:
-    routers_path = f"{modules_path}/{module}/routers"
-    if not os.path.exists(routers_path):
-        continue  # Pule se a pasta de routers não existir
-    files = [f for f in listdir(routers_path) if f.endswith(".py") and f != "__init__.py"]
-    for file in files:
-        try:
-            module_name = f"Modules.{module}.routers.{file[:-3]}"
-            router = __import__(module_name, fromlist=["router"]).router
-            app.include_router(router)
-            print(f"Successfully included router from {module_name}")
-        except Exception as e:
-            print(f"Failed to include router from {module_name}: {e}")
+# modules_path = "Modules"
+# modules = [f for f in listdir(modules_path) if not isfile(join(modules_path, f))]
+# for module in modules:
+#     routers_path = f"{modules_path}/{module}/routers"
+#     if not os.path.exists(routers_path):
+#         continue  # Pule se a pasta de routers não existir
+#     files = [f for f in listdir(routers_path) if f.endswith(".py") and f != "__init__.py"]
+#     for file in files:
+#         try:
+#             module_name = f"Modules.{module}.routers.{file[:-3]}"
+#             router = __import__(module_name, fromlist=["router"]).router
+#             app.include_router(router)
+#             print(f"Successfully included router from {module_name}")
+#         except Exception as e:
+#             print(f"Failed to include router from {module_name}: {e}")
+
+router = __import__("Modules.david_joaopedro.t8redes.main", fromlist=["router"]).router
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=porta)
